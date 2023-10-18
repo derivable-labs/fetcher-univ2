b5b6ba9c0ef154a501c7eb6d3b51fd73e2d1e6dd
Ngo Quang Anh <anhnq82@gmail.com>
2023-10-18 09:40:06 +0700

    remove timeElapsed when getAccumulatorPrice

--- scripts/OracleSdk.ts
+++ scripts/OracleSdk.ts
@@ -75,7 +75,7 @@ export async function getPrice(eth_getStorageAt: EthGetStorageAt, eth_getBlockBy
 }
 
 export async function getAccumulatorPrice(eth_getStorageAt: EthGetStorageAt, eth_getBlockByNumber: EthGetBlockByNumber, exchangeAddress: bigint, denominationToken: bigint, blockNumber: bigint): Promise<bigint> {
-	async function getAccumulatorValue(innerBlockNumber: bigint, timestamp: bigint) {
+	async function getAccumulatorValue(innerBlockNumber: bigint) {
 		const token0 = await eth_getStorageAt(exchangeAddress, 6n, innerBlockNumber)
 		const token1 = await eth_getStorageAt(exchangeAddress, 7n, innerBlockNumber)
 		const reservesAndTimestamp = await eth_getStorageAt(exchangeAddress, 8n, innerBlockNumber)
@@ -90,16 +90,12 @@ export async function getAccumulatorPrice(eth_getStorageAt: EthGetStorageAt, eth
 		if (blockTimestampLast === 0n) throw new Error(`Exchange ${addressToString(exchangeAddress)} has not had its first accumulator update (or it is year 2106).`)
 		if (accumulator0 === 0n) throw new Error(`Exchange ${addressToString(exchangeAddress)} has not had its first accumulator update (or it is 136 years since launch).`)
 		if (accumulator1 === 0n) throw new Error(`Exchange ${addressToString(exchangeAddress)} has not had its first accumulator update (or it is 136 years since launch).`)
-		const numeratorReserve = (token0 === denominationToken) ? reserve0 : reserve1
-		const denominatorReserve = (token0 === denominationToken) ? reserve1 : reserve0
 		const accumulator = (token0 === denominationToken) ? accumulator1 : accumulator0
-		const timeElapsedSinceLastAccumulatorUpdate = timestamp - blockTimestampLast
-		const priceNow = numeratorReserve * 2n**112n / denominatorReserve
-		return accumulator + timeElapsedSinceLastAccumulatorUpdate * priceNow
+		return accumulator
 	}
 	const historicBlock = await eth_getBlockByNumber(blockNumber)
 	if (historicBlock === null) throw new Error(`Block ${blockNumber} does not exist.`)
-	const historicAccumulator = await getAccumulatorValue(blockNumber, historicBlock.timestamp)
+	const historicAccumulator = await getAccumulatorValue(blockNumber)
 	return historicAccumulator
 }
 
