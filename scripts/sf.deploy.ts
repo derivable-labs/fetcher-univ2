dc766983c92d2f6c721a801dfb8882a5386547e1
Zergity <zergity@gmail.com>
2024-01-23 11:55:53 +0700

    estimateGas before deploy

--- scripts/sf.deploy.ts
+++ scripts/sf.deploy.ts
@@ -9,16 +9,23 @@ async function main() {
     const salt = 0
     const saltHex = ethers.utils.hexZeroPad(ethers.utils.hexlify(salt), 32)
     const SingletonFactoryABI = require('./abi/SingletonFactoryABI.json')
-    const url = hre.network.config.url
+    const { url, accounts, gasPrice } = hre.network.config
     // Connect to the network
     const provider = new ethers.providers.JsonRpcProvider(url)
     const singletonFactoryAddress = '0xce0042B868300000d44A59004Da54A005ffdcf9f'
     const contract = new ethers.Contract(singletonFactoryAddress, SingletonFactoryABI, provider)
-    const wallet = new ethers.Wallet(hre.network.config.accounts[0], provider)
+    const wallet = new ethers.Wallet(accounts[0], provider)
     const contractWithSigner = contract.connect(wallet)
-    opts.gasPrice = hre.network.config.gasPrice
-    console.log(wallet.address, opts)
+    if (gasPrice && gasPrice != 'auto') {
+        opts.gasPrice = gasPrice
+    }
     try {
+        const estimatedGas = (await contractWithSigner.estimateGas.deploy(bytecode, saltHex)).toNumber()
+        console.log('Estimated Gas:', estimatedGas.toLocaleString())
+        if (opts?.gasLimit < (estimatedGas * 3) >> 1) {
+            opts.gasLimit = (estimatedGas * 3) >> 1
+        }
+        console.log(wallet.address, opts)
         const deployTx = await contractWithSigner.deploy(bytecode, saltHex, opts)
         console.log('Tx: ', deployTx.hash)
         const res = await deployTx.wait(1)
